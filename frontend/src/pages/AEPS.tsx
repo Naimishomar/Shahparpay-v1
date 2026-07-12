8cd2e5839cb09714cca0bbca4e8f865624a20106
Naimish Omar <naimishomar@gmail.com>
2026-07-12 10:30:27 +0530

    Revert fType to 0 because WADH is sensitive to fType values

--- frontend/src/pages/AEPS.tsx
+++ frontend/src/pages/AEPS.tsx
@@ -237,7 +237,7 @@ const AEPS = () => {
             const wadhAttr = `wadh="${targetWadh}"`;
             const captureXml = `<?xml version="1.0"?>
             <PidOptions ver="1.0">
-            <Opts fCount="1" fType="2" iCount="0" pCount="0" format="0" pidVer="2.0" timeout="10000" env="P" ${wadhAttr} posh="UNKNOWN" />
+            <Opts fCount="1" fType="0" iCount="0" pCount="0" format="0" pidVer="2.0" timeout="10000" env="P" ${wadhAttr} posh="UNKNOWN" />
             <CustOpts>
                 <Param name="Param1" value="" />
             </CustOpts>
